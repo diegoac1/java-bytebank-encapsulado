23f19d5868c0dbd697cc8aa1f3ab1dd051174224
Diego <diegoacosta005@gmail.com>
2022-04-28 19:30:18 -0300

    first commit

---
+++ src/testaGetESet.java
@@ -0,0 +1,20 @@
+
+public class testaGetESet {
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		paulo.setNome("paulo silveira");
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		conta.getTitular().setProfissao("programador");
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+	}
+}
